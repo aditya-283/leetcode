5c250fa5ce448447db4508c82ec387117dd24e7e
razered <bhagwat.work@gmail.com>
2020-12-09 22:03:20 +0530

    Remaining changes

--- problems/Medium/coin-change/sol.py
+++ problems/Medium/coin-change/sol.py
@@ -23,3 +23,7 @@ sol = Solution()
 print(sol.coinChange(coins=[1, 9, 19], amount=100))
 
 
+ 1  2  3  4      13  9 5 1
+ 5  6  7  8  =>  14 10 6 2
+ 9 10 11 12      15 11 7 3
+13 14 15 16      16 12 8 4
