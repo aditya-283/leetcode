6542a2c7b5c28492da9bf7a1267482b11feccc15
razered <bhagwat.work@gmail.com>
2020-12-12 20:54:45 +0530

    Add solution.

---
+++ problems/contains-duplicate/sol.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+	def containsDuplicate(self, nums: List[int]) -> bool:
+		return len(set(nums)) != len(nums)
+
+print(Solution().containsDuplicate([1, 2, 3]))
