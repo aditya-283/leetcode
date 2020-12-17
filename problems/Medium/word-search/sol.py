fcca6356279e5085b12d467e0850127b7ecffa63
razered <bhagwat.work@gmail.com>
2020-12-17 16:50:27 +0530

    Improve time

--- problems/Medium/word-search/sol.py
+++ problems/Medium/word-search/sol.py
@@ -1,34 +1,27 @@
 from typing import List
-import copy
-
-from typing import List
-
-
-
 class Solution:
-	def existsFrom(self, board, seen, word, i, j):
+	def existsFrom(self, board, word, i, j):
 			if not word:
 				return True
 
-			if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
+			if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] != word[0]:
 				return False
 
-			if (i, j) not in seen and board[i][j] == word[0]:
-				return self.existsFrom(board, [(i, j)] + seen, word[1:], i+1, j) \
-						or self.existsFrom(board, [(i, j)] + seen, word[1:], i-1, j) \
-						or self.existsFrom(board, [(i, j)] + seen, word[1:], i, j-1) \
-						or self.existsFrom(board, [(i, j)] + seen, word[1:], i, j+1) 
-			else:
-				return False
+
+			tmp, board[i][j] = board[i][j], '#'
+			doesExist =  self.existsFrom(board, word[1:], i+1, j) \
+						or self.existsFrom(board, word[1:], i-1, j) \
+						or self.existsFrom(board, word[1:], i, j-1) \
+						or self.existsFrom(board, word[1:], i, j+1) 
+			board[i][j] = tmp
+			return doesExist
 
 	def exist(self, board: List[List[str]], word: str) -> bool:
-		exists = False
 		for i in range(len(board)):
 			for j in range(len(board[0])):
-				exists |= self.existsFrom(board, [], word, i, j)
-				if exists:
-					break
-		return exists
+				if self.existsFrom(board, word, i, j):
+					return True
+		return False
 
 
 print(Solution().exist(board=[["A","B","C","E"],
