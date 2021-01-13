e23290d34397d6f5e6d9da32714433ebf3df6dc8
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-13 14:29:21 +0530

    Complete solution.

--- problems/Easy/maximum-depth-of-binary-tree/sol.py
+++ problems/Easy/maximum-depth-of-binary-tree/sol.py
@@ -1,1 +1,14 @@
 from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+    	if not root:
+    		return 0
+    	else:
+    		return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
