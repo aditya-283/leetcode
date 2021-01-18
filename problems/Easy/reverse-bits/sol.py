b6428ec50c32dc1938f73a66f9ace62d55a1dc92
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-18 23:07:05 +0530

    Complete solution.

--- problems/Easy/reverse-bits/sol.py
+++ problems/Easy/reverse-bits/sol.py
@@ -1,1 +1,20 @@
 from typing import List
+
+def to_binary(n: int) -> str:
+	s = ''
+	while n:
+		s +=  str(n%2)
+		n //= 2
+	print(s)
+	return s[::-1]
+
+
+class Solution:
+	def reverseBits(self, n: int) -> int:
+		binary = to_binary(n)
+		binary = '0'*(32-len(binary)) + binary
+		return int(binary[::-1], 2)
+
+
+# print(to_binary(8))
+print(Solution().reverseBits(43261596))
