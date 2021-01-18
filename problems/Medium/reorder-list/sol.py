a195bf42ccd3db6eb75c7f9e45c114a643128d98
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-18 13:18:28 +0530

    Complete solution.

--- problems/Medium/reorder-list/sol.py
+++ problems/Medium/reorder-list/sol.py
@@ -1,1 +1,14 @@
 from typing import List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        
