1e7c0a2481e62b0d0d1eed2a1aeb77f6cb8e0d47
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-18 22:48:00 +0530

    Complete solution.

--- problems/Medium/find-minimum-in-rotated-sorted-array/sol.py
+++ problems/Medium/find-minimum-in-rotated-sorted-array/sol.py
@@ -1,1 +1,21 @@
 from typing import List
+
+class Solution:
+
+	def rotatedSearch(self, arr, start, end):
+		if start >= end:
+			return arr[start]
+		
+		mid = (start + end) // 2
+		if arr[mid] >= arr[0]:
+			return self.rotatedSearch(arr, mid+1, end)
+		elif arr[mid] < arr[0] and arr[mid] > arr[mid-1]:
+			return self.rotatedSearch(arr, start, mid-1)
+		elif arr[mid] < arr[0] and arr[mid] < arr[mid-1]:
+			return arr[mid]
+
+	def findMin(self, nums: List[int]) -> int:
+		return min(nums[0], self.rotatedSearch(nums, 0, len(nums)-1))
+
+
+print(Solution().findMin([11,13,15,17]))
