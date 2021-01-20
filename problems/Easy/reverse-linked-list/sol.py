378d4750ac265cd01bd657e96c687053b7137cac
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-20 13:58:39 +0530

    Complete solution.

--- problems/Easy/reverse-linked-list/sol.py
+++ problems/Easy/reverse-linked-list/sol.py
@@ -1,1 +1,21 @@
 from typing import List
+
+# Definition for singly-linked list.
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+class Solution:
+	def reverseList(self, head: ListNode) -> ListNode:
+		if not head:
+			return None
+		it = head.next
+		first = head
+		head.next = None
+		while it:
+			next = it.next
+			it.next = first
+			first = it
+			it = next
+		return first
--- problems/Medium/course-schedule/sol.py
+++ problems/Medium/course-schedule/sol.py
@@ -1,1 +1,40 @@
 from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+	def edgelist_to_adjlist(self, num_nodes, edgelist):
+		adjlist = [[] for _ in range(num_nodes)]
+		for edge in edgelist:
+			# print(edge)
+			adjlist[edge[0]].append(edge[1])
+			# print()
+		# print('Here: ', str(adjlist))
+		return adjlist
+
+	def hasCycle(self, adjlist):
+		def dfs_visit(v):
+			discovered.add(v)
+			for nbr in adjlist[v]:
+				if nbr not in discovered and nbr not in finished:
+					dfs_visit(nbr)
+				elif nbr in discovered:
+					back_edges.append((v, nbr))
+			finished.add(v)
+			discovered.remove(v)
+
+		discovered = set()
+		finished = set()
+		back_edges = []
+		for v in range(len(adjlist)):
+			if v not in discovered and v not in finished:
+				dfs_visit(v)
+		return len(back_edges) > 0
+
+
+	def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+		adjlist = self.edgelist_to_adjlist(numCourses, prerequisites)
+		return not self.hasCycle(adjlist)
+
+
+
+print(Solution().canFinish(2, [[1,0],[0,1]]))
