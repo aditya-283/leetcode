39de632d26e3adbf96b4604915eeb9188ed0c8e9
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-21 13:44:52 +0530

    Complete solution.

--- problems/Medium/design-add-and-search-words-data-structure/sol.py
+++ problems/Medium/design-add-and-search-words-data-structure/sol.py
@@ -4,38 +4,15 @@ class WordDictionary:
 	def __init__(self):
 		self.data = {}
 
-	# def traverse(self, word: str):
-	# 	iter = self
-	# 	while word:
-	# 		if word[0] in iter.data:
-	# 			iter = iter.data[word[0]]
-	# 			word = word[1:]
-	# 		else:
-	# 			break
-
-	# 	return iter, word
-
-	# def insert(self, word: str) -> None:
-	# 	word = word + '$'
-	# 	final_match, remainder = self.traverse(word)
-	# 	iter = final_match
-	# 	for c in remainder:
-	# 		iter.data[c] =  WordDictionary()
-	# 		iter = iter.data[c]
-
-
 	def addWord(self, word):
 		word = word + '$'
-		trie = self.data
+		trie = self
 		for c in word:
-			if c in trie:
-				trie = trie[c]
+			if c in trie.data:
+				trie = trie.data[c]
 			else:
-				trie[c] = {}
-				trie = trie[c]
-		
-		# return self.insert(word)
-
+				trie.data[c] = WordDictionary()
+				trie = trie.data[c]
 
 	def searchRec(self, word):
 		if not word:
@@ -54,10 +31,10 @@ class WordDictionary:
 
 
 
-obj = Trie()
-print(obj.insert("apple"))
+obj = WordDictionary()
+print(obj.addWord("apple"))
 print(obj.search("apple"))
 print(obj.search("app"))
-print(obj.insert("app"))
+print(obj.addWord("app"))
 print(obj.search("app"))
-print(obj.search("..p.r"))
+print(obj.search("..p.e"))
