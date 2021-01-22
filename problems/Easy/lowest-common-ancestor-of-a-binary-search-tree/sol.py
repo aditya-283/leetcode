7c16c0e8dead83a1fb050c86ff66eb58abd42f79
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-22 13:11:37 +0530

    Complete solution.

--- problems/Easy/lowest-common-ancestor-of-a-binary-search-tree/sol.py
+++ problems/Easy/lowest-common-ancestor-of-a-binary-search-tree/sol.py
@@ -1,1 +1,34 @@
 from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+	def __init__(self, x, left=None, right=None):
+		self.val = x
+		self.left = left
+		self.right = right
+
+class Solution:
+	def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+		if p == q:
+			return p
+		elif p.val <= root.val <= q.val or p.val >= root.val >= q.val:
+			return root
+		elif p.val < root.val:
+			return self.lowestCommonAncestor(root.left, p, q)
+		else:
+			return self.lowestCommonAncestor(root.right, p, q)
+
+
+tree =	TreeNode(6,
+			TreeNode(2, 
+						TreeNode(0),
+						TreeNode(4, 
+									TreeNode(3),
+									TreeNode(5))),
+			TreeNode(8, 
+						TreeNode(7),
+						TreeNode(9)))
+p = tree.left
+q = tree.left.right
+# print(p.val, q.val)
+print(Solution().lowestCommonAncestor(tree, p, q).val)
--- problems/Medium/lowest-common-ancestor-of-a-binary-tree/sol.py
+++ problems/Medium/lowest-common-ancestor-of-a-binary-tree/sol.py
@@ -1,1 +1,69 @@
 from typing import List
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+	def __init__(self, x, left=None, right=None):
+		self.val = x
+		self.left = left
+		self.right = right
+
+class Solution:
+	def lowestCommonAncestor(self, root, p, q):
+		discovered = set()
+		parent = {}
+
+		def ancestors(v):
+			iter = v
+			ancestors = [iter]
+			while parent[iter] != -1:
+				iter = parent[iter]
+				ancestors.append(iter)
+			return reversed(ancestors)
+
+		def lastmatch(l1, l2):
+			for x, y in zip(l1, l2):
+				if x == y:
+					match = x
+			return match
+
+		def bfs(s):
+			q = deque()
+			q.appendleft(s)
+			parent[s] = -1
+			while q:
+				top = q.pop()
+				if top.left:
+					parent[top.left] = top
+					q.appendleft(top.left)
+				if top.right:
+					parent[top.right] = top
+					q.appendleft(top.right)
+
+		bfs(root)
+		return lastmatch(ancestors(p), ancestors(q))
+
+
+	def lowestCommonAncestorInefficient(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+		def lastmatch(l1, l2):
+			for x, y in zip(l1, l2):
+				if x == y:
+					match = x
+			return match
+
+		def dfs(v, x, open):
+			left, right = None, None
+			if v and v != x:
+				left = dfs(v.left, x, open + [v]) 
+				right = dfs(v.right, x, open + [v])
+			elif v == x:
+				return open + [v]
+			return left or right
+
+		return lastmatch(dfs(root, p, []), dfs(root, q, []))
+
+
+
+
+tree = TreeNode(3, TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), TreeNode(4))), TreeNode(1, TreeNode(0), TreeNode(8)))
+print(Solution().lowestCommonAncestor(tree, tree.left, tree.left.right.right).val)
