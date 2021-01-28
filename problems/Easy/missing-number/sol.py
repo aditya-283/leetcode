a9de55f12e40bb02509c77b7759f48e089e76bf9
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-28 11:57:06 +0530

    Complete solution.

--- problems/Easy/missing-number/sol.py
+++ problems/Easy/missing-number/sol.py
@@ -1,1 +1,9 @@
 from typing import List
+
+class Solution:
+	def missingNumber(self, nums: List[int]) -> int:
+		n = len(nums)
+		return int(n*(n+1)/2) - sum(nums)
+
+
+print(Solution().missingNumber([9,6,4,2,3,5,7,0,1]))
