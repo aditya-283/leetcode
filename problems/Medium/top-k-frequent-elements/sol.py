b9b77ccab62cbc746266bba42314861e9f0c8f7b
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-29 22:39:46 +0530

    Complete solution.

--- problems/Medium/top-k-frequent-elements/sol.py
+++ problems/Medium/top-k-frequent-elements/sol.py
@@ -1,1 +1,9 @@
 from typing import List
+from collections import defaultdict
+from heapq import heappush, heappop, heapreplace, nlargest
+class Solution:
+	def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+		return [x[1] for x in nlargest(k, [(value, key) for key, value in Counter(nums).items()])]
+
+
+print(Solution().)
