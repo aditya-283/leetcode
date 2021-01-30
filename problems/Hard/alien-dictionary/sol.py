f6ee92e602465a60ee4ebbf7090279a9d07419f6
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-01-30 17:17:31 +0530

    Complete solution.

--- problems/Hard/alien-dictionary/sol.py
+++ problems/Hard/alien-dictionary/sol.py
@@ -1,1 +1,53 @@
 from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+	def alienOrder(self, words: List[str]) -> str:
+		def topo(adjlist):
+			def dfs_visit(s):
+				opened.add(s)
+				for nbr in adjlist[s]:
+					if nbr not in opened and nbr not in finished:
+						if not dfs_visit(nbr):
+							return False
+					elif nbr in opened:
+						ancestors = list(opened)
+						return False
+				opened.remove(s)
+				finished.add(s)
+				topo.appendleft(s)
+				return True
+
+			opened, finished, topo = set(), set(), deque()
+			for v in adjlist.keys():
+				if v not in finished:
+					if not dfs_visit(v):
+						return []
+			return list(topo)
+
+		adjlist = {c: [] for c in set(''.join(words))}
+		for cur, nxt in zip(words, words[1:]):
+			if len(cur) > len(nxt) and cur[:len(nxt)] == nxt:
+				return ''
+
+			for i, j in zip(cur, nxt):
+				if i != j:
+					if j not in adjlist[i]:
+						adjlist[i].append(j)
+					break
+
+		return ''.join(topo(adjlist))
+
+a = Solution()
+# print(a.alienOrder(["ac","ab","zc","zb"]))
+# print(a.alienOrder(["wrt","wrf","er","ett","rftt"]))
+print(a.alienOrder(["wrt","wrf","er","ett","rftt","te"]))
+# print(a.alienOrder(["abc","ab"]))
+# print(a.alienOrder(["wrt","wrtkj"]))
+# print(a.alienOrder(["dvpzu","bq","lwp","akiljwjdu","vnkauhh","ogjgdsfk","tnkmxnj","uvwa","zfe","dvgghw","yeyruhev","xymbbvo","m","n"]))
+
+
+# BITCH SIMPLIFY
+# IF YOU NEED AN ADJLIST FOR TOPO DONT MAKE AN EDGELIST FIRST AND THEN CONVERT
+# MINIMISE CODE. SIMPLIFYYYYY 
+# FUNCTIONS ONLY MAKE SENSE IF THERE REALLY IS MODULARITY REQD. DONT OVERDO FUNCTIONS.
--- problems/Hard/find-median-from-data-stream/sol.py
+++ problems/Hard/find-median-from-data-stream/sol.py
@@ -1,1 +1,57 @@
 from typing import List
+from heapq import heappush, heappop
+
+class MedianFinder:
+	def __init__(self):
+		self.left_max_heap = []
+		self.right_min_heap = []
+		self.middle = None # only if odd number of elements
+
+	def addNum(self, num: int) -> None:
+		if self.middle is not None and num < self.middle:
+			heappush(self.left_max_heap, -num)
+			heappush(self.right_min_heap, self.middle)
+			self.middle = None
+		elif self.middle is not None and num >= self.middle:
+			heappush(self.left_max_heap, -self.middle)
+			heappush(self.right_min_heap, num)
+			self.middle = None
+		elif not self.left_max_heap:
+			self.middle = num
+		elif num < -self.left_max_heap[0]:
+			self.middle = -heappop(self.left_max_heap)
+			heappush(self.left_max_heap, -num)
+		elif num < self.right_min_heap[0]:
+			self.middle = num
+		else:
+			self.middle = heappop(self.right_min_heap)
+			heappush(self.right_min_heap, num)
+
+
+	def findMedian(self) -> float:
+		if self.middle:
+			return self.middle
+		else:
+			return (-self.left_max_heap[0] + self.right_min_heap[0]) / 2
+		
+
+
+		
+
+# BITCH BE CAREFUL WITH EDGECASES
+# NEW GOAL - DONT LOOK AT THE TEST CASE THAT FAILED.
+
+
+# Your MedianFinder object will be instantiated and called as such:
+obj = MedianFinder()
+obj.addNum(40)
+ans1 = obj.findMedian()
+obj.addNum(12)
+print(obj.left_max_heap, '--', obj.middle, '--', obj.right_min_heap)
+ans2 = obj.findMedian()
+obj.addNum(16)
+print(obj.left_max_heap, '--', obj.middle, '--', obj.right_min_heap)
+ans3 = obj.findMedian()
+print(obj.left_max_heap, '--', obj.middle, '--', obj.right_min_heap)
+print(ans3)
+
