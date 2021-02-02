9ba7bf217b0c752c6f5c5f4914da37c5d9082ba9
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-02-02 11:33:21 +0530

    Complete solution.

--- problems/Hard/best-time-to-buy-and-sell-stock-iii/sol.py
+++ problems/Hard/best-time-to-buy-and-sell-stock-iii/sol.py
@@ -1,1 +1,14 @@
 from typing import List
+from itertools import accumulate
+
+class Solution:
+	# OK O(n^2) algorithm, can be made O(n)
+	def maxProfit(self, prices: List[int]) -> int:
+		n = len(prices)
+		min_until = list(accumulate(prices, min))
+		max_from = list(reversed(list(accumulate(reversed(prices), max))))
+		profits_till_i = list(accumulate([prices[i] - min_until[i] for i in range(n)], max))
+		profits_from_i = list(reversed(list(accumulate(reversed([max_from[i] - prices[i] for i in range(n)]), max))))
+		return max(profits_till_i[i] + profits_from_i[i] for i in range(n))
+
+print(Solution().maxProfit([1,2,3,4,5]))
