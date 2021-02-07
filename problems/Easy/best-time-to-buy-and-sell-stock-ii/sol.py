e17f8f0d35eefd9a4e02327f5bca34fc68bb99c6
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-02-07 22:40:08 +0530

    Complete solution.

--- problems/Easy/best-time-to-buy-and-sell-stock-ii/sol.py
+++ problems/Easy/best-time-to-buy-and-sell-stock-ii/sol.py
@@ -1,1 +1,8 @@
 from typing import List
+
+class Solution:
+	def maxProfit(self, prices: List[int]) -> int:
+		return sum([nxt-cur for cur, nxt in zip(prices, prices[1:]) if nxt > cur])
+
+
+print(Solution().maxProfit([7,1,5,3,6,4]))
