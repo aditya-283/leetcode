b37eaadbfdbf5d4e0a6c8670a63dddf09209e3cf
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-02-07 22:26:18 +0530

    Complete solution.

--- problems/Medium/best-time-to-buy-and-sell-stock-iv/sol.py
+++ problems/Medium/best-time-to-buy-and-sell-stock-iv/sol.py
@@ -1,2 +1,22 @@
 from typing import List
-from typing import List
+from functools import cache
+
+
+class Solution:
+	def maxProfit(self, k: int, prices: List[int]) -> int:
+		@cache
+		def profitHelper(k, pos, buy):
+			if not prices or not k or pos >= len(prices):
+				return 0
+
+			if buy:
+				return max(profitHelper(k, pos+1, 0) - prices[pos], profitHelper(k, pos+1, 1))
+			else:
+				return max(profitHelper(k-1, pos+1, 1) + prices[pos], profitHelper(k, pos+1, 0))
+
+		return profitHelper(k, 0, 1)
+
+
+
+
+print(Solution().maxProfit(2, [1, 3, 5, 2, 5, 1, 6]))
