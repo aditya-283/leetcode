22c2c370f60ece19755f9aaedd452690287d1b35
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-03-27 13:31:16 +0530

    Complete solution.

--- problems/Easy/binary-tree-paths/sol.py
+++ problems/Easy/binary-tree-paths/sol.py
@@ -1,1 +1,25 @@
+# Definition for a binary tree node.
 from typing import List
+
+
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+
+class Solution:
+	def binaryTreePaths(self, root: TreeNode) -> List[str]:
+		if not root.right and not root.left: 
+			return [f'{root.val}']
+
+		childPaths = []
+		if root.right:
+			childPaths.extend(self.binaryTreePaths(root.right))
+		if root.left:
+			childPaths.extend(self.binaryTreePaths(root.left))
+		return [f'{root.val}->{path}' for path in childPaths]
+
+
+print(Solution().binaryTreePaths(TreeNode(1, TreeNode(2, None, TreeNode(5)), TreeNode(3))))
--- problems/Hard/remove-invalid-parentheses/sol.py
+++ problems/Hard/remove-invalid-parentheses/sol.py
@@ -1,1 +1,7 @@
 from typing import List
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+
+
+        
