441a528383973d6d6bcdad28a00433b0e65d04e7
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-03-27 13:40:25 +0530

    Complete solution.

--- problems/Easy/first-bad-version/sol.py
+++ problems/Easy/first-bad-version/sol.py
@@ -1,1 +1,25 @@
 from typing import List
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+	def firstBadVersion(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		def firstBadVersionBinSearch(left, right):
+			mid = (left + right) // 2
+			if isBadVersion(mid):
+				if isBadVersion(mid-1):
+					return firstBadVersionBinSearch(left, mid-1)
+				else:
+					return mid
+			elif not isBadVersion(mid):
+				return firstBadVersionBinSearch(mid+1, right)
+				
+		return firstBadVersionBinSearch(1, n)
