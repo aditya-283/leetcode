9c3cf9db1873614584a42baf7cc3dbb59b7a7371
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-03-27 14:29:47 +0530

    Complete solution.

--- problems/Easy/move-zeroes/sol.py
+++ problems/Easy/move-zeroes/sol.py
@@ -1,1 +1,20 @@
 from typing import List
+
+
+class Solution:
+	def moveZeroes(self, nums: List[int]) -> None:
+		"""
+		Do not return anything, modify nums in-place instead.
+		"""
+		i, zeroAt = 0, -1
+		while i < len(nums):
+			if zeroAt == -1 and nums[i] == 0:
+					zeroAt = i
+			elif nums[i] != 0 and zeroAt >= 0:
+				nums[zeroAt], nums[i] = nums[i], nums[zeroAt]
+				zeroAt += 1
+			i += 1
+		return nums
+
+
+print(Solution().moveZeroes([1, 0, 1]))
