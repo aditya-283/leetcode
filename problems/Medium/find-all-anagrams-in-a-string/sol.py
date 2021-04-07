932e716477f50f5416e6e17d6cbf9a5ebc546efc
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-07 15:48:10 +0530

    Complete solution.

--- problems/Medium/find-all-anagrams-in-a-string/sol.py
+++ problems/Medium/find-all-anagrams-in-a-string/sol.py
@@ -1,1 +1,20 @@
 from typing import List
+from collections import Counter
+
+class Solution:
+	def findAnagrams(self, s: str, p: str) -> List[int]:
+		i, j, n, ans = 0, len(p), len(s), []
+		s_counter, p_counter = Counter(s[i:j]), Counter(p)
+		while i <= len(s) - len(p):
+			if s_counter == p_counter:
+				ans.append(i)
+			s_counter[s[i]] -= 1
+			if s_counter[s[i]] == 0:
+				del s_counter[s[i]]
+			i += 1
+			if j < len(s):
+				s_counter[s[j]] += 1
+				j += 1
+		return ans
+
+print(Solution().findAnagrams("cbaebabacd", "abc"))
