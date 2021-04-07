868582d4c3f5fc914810acd9248dae2c8d28d1a1
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-07 20:54:33 +0530

    Complete solution.

--- problems/Medium/continuous-subarray-sum/sol.py
+++ problems/Medium/continuous-subarray-sum/sol.py
@@ -1,1 +1,13 @@
 from typing import List
+from itertools import accumulate
+
+
+class Solution:
+	def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+		existsKMultiplePair = any((i, j) for i, j in zip(nums, nums[1:]) if i % k == 0 and j % k == 0)
+		c_sums = list(accumulate([num for num in nums if num % k]))
+		c_mods = [x % k for x in c_sums]
+		return existsKMultiplePair or (len(set(c_mods)) < len(c_sums)) or (0 in c_mods)
+
+
+print(Solution().checkSubarraySum([5,0,0,0], 3))
