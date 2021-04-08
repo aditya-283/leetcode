a53e89dc6c3f897f9a94c590fb0e54209b18c54b
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-08 15:16:17 +0530

    Complete solution.

--- problems/Medium/subarray-sum-equals-k/sol.py
+++ problems/Medium/subarray-sum-equals-k/sol.py
@@ -1,1 +1,15 @@
 from typing import List
+from itertools import accumulate
+from collections import defaultdict
+
+class Solution:
+	def subarraySum(self, nums: List[int], k: int) -> int:
+		result, sum_, hashmap = 0, 0, defaultdict(int)
+		hashmap[0] = 1
+		for num in nums:
+			sum_ += num
+			if sum_ - k in hashmap:
+				result += hashmap[sum_ - k]
+			hashmap[sum_] += 1
+		return result
+print(Solution().subarraySum([1,2,3], 3))
