c15e49389fe23c385a9246fc9c7e8aa81b31d0d6
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-09 12:36:25 +0530

    Complete solution.

--- problems/Medium/custom-sort-string/sol.py
+++ problems/Medium/custom-sort-string/sol.py
@@ -1,1 +1,8 @@
 from typing import List
+
+class Solution:
+	def customSortString(self, S: str, T: str) -> str:
+		order_ = {c: i for i, c in enumerate(S)}
+		return ''.join(sorted(T, key=lambda x: order_[x] if x in order_ else 99))
+
+print(Solution().customSortString("cba", "abcd"))
