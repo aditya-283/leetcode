552ab5061539bea5d180d3c2541b39a968642004
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-11 14:26:52 +0530

    Complete solution.

--- problems/Medium/k-closest-points-to-origin/sol.py
+++ problems/Medium/k-closest-points-to-origin/sol.py
@@ -1,1 +1,7 @@
 from typing import List
+
+class Solution:
+	def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+		return sorted(points, key=lambda x: x[0]**2 + x[1]**2)[:k]
+
+print(Solution().kClosest([[3,3],[5,-1],[-2,4]], 2))
