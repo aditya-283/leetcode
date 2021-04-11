53f301d09a375eac1a29a87f68ad71729620067b
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-11 18:42:45 +0530

    Complete solution.

--- problems/Medium/minimum-remove-to-make-valid-parentheses/sol.py
+++ problems/Medium/minimum-remove-to-make-valid-parentheses/sol.py
@@ -1,1 +1,19 @@
 from typing import List
+
+class Solution:
+	def minRemoveToMakeValid(self, s: str) -> str:
+		parens = []
+		remove = []
+		for i, c in enumerate(s):
+			if c == '(':
+				parens.append(i)
+			elif c == ')':
+				if not parens:
+					remove.append(i)
+				else:
+					parens.pop()
+		remove.extend(parens)
+		return ''.join([c for i, c in enumerate(s) if i not in remove])
+
+
+print(Solution().minRemoveToMakeValid("a)b(c)d"))
