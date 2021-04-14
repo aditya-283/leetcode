77883500abfdca786149e60425cd8167bbbd8860
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-15 01:06:58 +0530

    Complete solution.

--- problems/Easy/rank-transform-of-an-array/sol.py
+++ problems/Easy/rank-transform-of-an-array/sol.py
@@ -1,1 +1,15 @@
 from typing import List
+
+class Solution:
+	def arrayRankTransform(self, arr: List[int]) -> List[int]:
+		inOrder = sorted(arr)
+		ranks = {}
+		count = 0
+		prev = float('-inf')
+		for num in inOrder:
+			if num > prev:
+				count += 1
+				prev = num
+			ranks[num] = count
+			
+		return [ranks[i] for i in arr]
