e4beeb7cc3a910530598e10f2f7e46e926fe1cee
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-18 17:08:04 +0530

    Add solution to synonymous-sentences/

--- problems/Medium/synonymous-sentences/sol.py
+++ problems/Medium/synonymous-sentences/sol.py
@@ -1,1 +1,18 @@
 from typing import List
+
+
+class Solution:
+	def doesCircleExist(commands):
+		answers = []
+		for command in commands:
+			net_rotation = abs(command.count('L') - command.count('R'))
+			if net_rotation == 0 and command.count('G') == 0:
+				answers.append('YES')
+			elif net_rotation == 0 and command.count('G') != 0:
+				answers.append('NO')
+			elif net_rotation % 4 != 0:
+				answers.append('YES')
+			else:
+				answers.append('NO') 
+		
+from typing import List
