2be81fb21a642cf8bb47eda8c1e038f43e6feeb9
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-25 12:46:02 +0530

    Complete solution.

--- problems/Easy/majority-element/sol.py
+++ problems/Easy/majority-element/sol.py
@@ -1,1 +1,19 @@
 from typing import List
+
+class Solution:
+	def majorityElement(self, nums: List[int]) -> int:
+		x = nums[0]
+		count = 1
+		for num in nums:
+			if num == x:
+				count += 1
+			else:
+				count -= 1
+
+			if count == 0:
+				x = num
+				count = 1
+		return x if nums.count(x) > len(nums) // 2 else -1
+				
+
+print(Solution().majorityElement([2,2,1,1,1,2,2]))
