4048162e5d466bdb7ac3a11b8bb76618bd7c3e05
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-15 21:19:16 +0530

    Complete solution.

--- problems/Hard/remove-invalid-parentheses/sol.py
+++ problems/Hard/remove-invalid-parentheses/sol.py
@@ -1,14 +1,41 @@
 from typing import List
 from collections import deque
 from itertools import product
-
+from math import inf
 class Solution:
 	def removeInvalidParentheses(self, s: str) -> List[str]:
-		removeChoices = set()
-		
-		
+		def nbrs(s):
+			return set(s[:i] + s[i+1:] for i in range(len(s)) if s[i] in ('(', ')'))
+
+		def isValid(s):
+			a = deque()
+			for c in s:
+				if c == '(':
+					a.append(c)
+				elif c == ')':
+					if a:
+						a.pop()
+					else:
+						return False
+			return not a
+
+		maxDepth = inf
+		q, visited = deque([(s, 0)]), set([s])
+		ans = []
+		while q:
+			cur, depth = q.pop()
+			if isValid(cur):
+				maxDepth = min(maxDepth, len(s) - len(cur))
+				ans.append(cur)
+				continue
+			if depth < maxDepth or depth == 0:
+				for nbr in nbrs(cur):
+					if nbr not in visited:
+						visited.add(nbr)
+						q.appendleft((nbr, depth + 1))
+		return ans
 
 
-print(Solution().removeInvalidParentheses(")("))
+# print(Solution().removeInvalidParentheses("(())"))
 
-from typing import List
+print(Solution().removeInvalidParentheses("(a)())()"))
--- problems/Medium/magnetic-force-between-two-balls/sol.py
+++ problems/Medium/magnetic-force-between-two-balls/sol.py
@@ -1,1 +1,31 @@
 from typing import List
+from bisect import bisect_left
+
+class Solution:
+	def maxDistance(self, position: List[int], m: int) -> int:
+		n = len(position)
+		position.sort()
+		def isPossible(d):
+			last = position[0]
+			for i in range(m-1):
+				idx = bisect_left(position, last + d)
+				if (idx >= n):
+					return False
+				last = position[idx]
+			return True
+
+		lower = min(nxt - prev for prev, nxt in zip(position, position[1:]))
+		upper = (position[-1] - position[0])//(m - 1)
+		cand = upper
+		while True:
+			if isPossible(cand):
+				lower = cand
+			else:
+				upper = cand
+			cand = (lower + upper)//2
+			if cand == lower:
+				break
+		return lower
+
+
+print(Solution().maxDistance(position = [5,4,3,2,1,1000000000], m = 2))
