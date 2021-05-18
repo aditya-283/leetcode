1bc8e0c320a53a394a0e4c867bc44a27c2e21ad5
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-18 20:40:23 +0530

    Complete solution.

--- problems/Medium/evaluate-division/sol.py
+++ problems/Medium/evaluate-division/sol.py
@@ -1,1 +1,51 @@
 from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+	def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+		adjlist = defaultdict(list)
+		for (var1, var2), value in zip(equations, values):
+			adjlist[var1].append((var2, 1/value))
+			adjlist[var2].append((var1, value))
+
+
+
+		visited = set()
+		ratios = {}
+		roots = {}
+		def bfs(root):
+			roots[root] = root
+			ratios[root] = {}
+			ratios[root][root] = 1
+			q = deque([(root, 1, 0)])
+			visited.add(root)
+			while q:
+				cur, acc, par = q.pop()
+				ratios[root][cur] = acc
+				roots[cur] = root
+				for nbr, weight in adjlist[cur]:
+					if nbr not in visited:
+						q.appendleft((nbr, acc*weight, cur))
+						visited.add(nbr)
+					elif nbr != par and nbr in ratios[root] and acc*weight != ratios[root][nbr]:
+						ratios[root][nbr] = -1  # bad cycle detected. if acc*weight == ratios[root][nbr] then its an allowed cycle
+
+
+		for node in adjlist:
+			if node not in visited:
+				bfs(node)
+
+		ans = []
+		for u, v in queries:
+			if u not in roots or v not in roots:
+				ans.append(float(-1))
+			elif roots[u] != roots[v]: # belong to different components
+				ans.append(float(-1))
+			else:
+				root = roots[u]
+				x = ratios[root][u] # u = x.root
+				y = ratios[root][v] # v = y.root
+				ans.append(x/y)
+		return ans
+
+print(Solution().calcEquation(equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]))
--- problems/Medium/longest-increasing-subsequence/sol.py
+++ problems/Medium/longest-increasing-subsequence/sol.py
@@ -1,1 +1,17 @@
 from typing import List
+from bisect import bisect_left
+
+class Solution:
+	def lengthOfLIS(self, nums: List[int]) -> int:
+		n = len(nums)
+		dp = [1]*n
+		for i in range(n):
+			for j in range(i):
+				if nums[j] < nums[i]:
+					dp[i] = max(dp[i], dp[j] + 1)
+		return max(dp)
+			
+
+
+
+print(Solution().lengthOfLIS(nums = [10,9,2,5,3,7,101,18]))
