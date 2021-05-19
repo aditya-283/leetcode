e73364349b4c92d4982b489fe62b9a3ef84c2d25
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-20 01:33:24 +0530

    Add solution to largest-rectangle-in-histogram/

--- problems/Hard/largest-rectangle-in-histogram/sol.py
+++ problems/Hard/largest-rectangle-in-histogram/sol.py
@@ -1,25 +1,51 @@
 from typing import List
-
+from collections import deque
 class Solution:
-	def largestRectangleArea(self, heights: List[int]) -> int:
-		stack = [-1]
-		max_area = 0
-		for i in range(len(heights)):
-			while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
-				print(i, [heights[j] for j in stack])
-				current_height = heights[stack.pop()]
-				current_width = i - stack[-1] - 1
-				print(i, [heights[j] for j in stack])
-				print(heights[i], '->', current_width, current_height)
-				max_area = max(max_area, current_height * current_width)
-			stack.append(i)
+	def largestRectangleArea(self, heights):
+		stack = deque()
+		index, maxArea = 0, 0
+		while index < len(heights):
+			if not stack or heights[index] >= heights[stack[-1]]:
+				stack.append(index)
+				index += 1
+			else:
+				height = heights[stack.pop()]
+				width = (index - 1 - stack[-1]) if stack else index
+				area = height * width
+				maxArea = max(area, maxArea)
+		while stack:
+				height = heights[stack.pop()]
+				width = (index - 1 - stack[-1]) if stack else index
+				area = height * width
+				maxArea = max(area, maxArea)
+		return maxArea
+  
+# print(Solution().largestRectangleArea([2,1,5,6,2,3]))
 
-		print([heights[i] for i in stack[1:]])
-		while stack[-1] != -1:
-			current_height = heights[stack.pop()]
-			current_width = len(heights) - stack[-1] - 1
 
-			max_area = max(max_area, current_height * current_width)
-		return max_area
- 
-print(Solution().largestRectangleArea([2,1,5,6,2,3]))from typing import List
+
+# 4 functions into 1
+def adjacentElem(arr, fromStart=True, lessThan=True):
+	def compare(x, y):
+		return arr[stack[-1]] > arr[index] if lessThan else arr[stack[-1]] < arr[index] 
+
+	iterator = iter(range(len(arr))) if fromStart else iter(range(len(arr)-1, -1, -1))
+	stack = deque()
+	index = next(iterator)
+	lastLessThan = [-1]*len(arr)
+	try:
+		while True:
+			if stack and compare(arr[stack[-1]], arr[index]):
+				stack.pop()
+			else:
+				lastLessThan[index] = stack[-1] if stack else -1
+				stack.append(index)
+				index = next(iterator)
+	except StopIteration:
+		print("Python's iterator API is not pretty :(")
+	finally:
+		return lastLessThan
+
+
+print(adjacentElem([2,1,5,6,2,3], True, False))
+from typing import List
