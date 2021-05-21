45a72a0851ffeff1dd003ed3f9ca78fb24ed60df
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-21 21:14:46 +0530

    Complete solution.

--- problems/Easy/logger-rate-limiter/sol.py
+++ problems/Easy/logger-rate-limiter/sol.py
@@ -1,1 +1,20 @@
 from typing import List
+
+class Logger:
+
+	def __init__(self):
+		self.lastReceivedAt = {}
+		
+
+	def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+		if message in self.lastReceivedAt and self.lastReceivedAt[message] + 10 > timestamp:
+				return False
+		self.lastReceivedAt[message] = timestamp
+		return True
+
+		
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
--- problems/Medium/bulls-and-cows/sol.py
+++ problems/Medium/bulls-and-cows/sol.py
@@ -1,1 +1,24 @@
 from typing import List
+from collections import defaultdict
+
+class Solution:
+	def getHint(self, secret: str, guess: str) -> str:
+		bull_indices = set()
+		for i, (s, g) in enumerate(zip(secret, guess)):
+			if s == g:
+				bull_indices.add(i)
+
+		non_bull_guess_chars = defaultdict(int)
+		for i, c in enumerate(guess):
+			if i not in bull_indices:
+				non_bull_guess_chars[c] += 1
+
+		cows = 0
+		for i, c in enumerate(secret):
+			if i not in bull_indices and c in non_bull_guess_chars and non_bull_guess_chars[c] > 0:
+				non_bull_guess_chars[c] -= 1
+				cows += 1
+
+		return f"{len(bull_indices)}A{cows}B"
+
+print(Solution().getHint(secret = "1", guess = "1"))
