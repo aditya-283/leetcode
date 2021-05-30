18faf7bf0276fcdd6165da365b9a0b3f7afd91d8
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-30 14:57:01 +0530

    Complete solution.

--- problems/Medium/rle-iterator/sol.py
+++ problems/Medium/rle-iterator/sol.py
@@ -1,1 +1,20 @@
 from typing import List
+
+class RLEIterator:
+
+	def __init__(self, encoding: List[int]):
+		self.pointer = 0
+		self.encoding = encoding
+
+	def next(self, n: int) -> int:
+		if self.pointer >= len(self.encoding):
+			return -1
+		while n > self.encoding[self.pointer]:
+			n -= self.encoding[self.pointer]
+			self.pointer += 2
+			if self.pointer >= len(self.encoding):
+				return -1
+		self.encoding[self.pointer] -= n
+		ans = self.encoding[self.pointer+1]
+		return ans
+
