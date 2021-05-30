4910350e4c70efbb8c8e1d19541399d07f9bbb41
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-30 13:50:39 +0530

    Complete solution.

--- problems/Medium/flip-equivalent-binary-trees/sol.py
+++ problems/Medium/flip-equivalent-binary-trees/sol.py
@@ -1,1 +1,19 @@
 from typing import List
+
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+		
+class Solution:
+	@cache
+	def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+		if not root1 and not root2:
+			return True
+		elif not root1 or not root2:
+			return False
+		else:
+			return root1.val == root2.val and ((self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right))
+									      or (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)))
+
--- problems/Medium/sentence-screen-fitting/sol.py
+++ problems/Medium/sentence-screen-fitting/sol.py
@@ -1,1 +1,38 @@
 from typing import List
+from functools import cache
+from bisect import bisect_left
+
+class Solution:
+	def wordsTyping(self, sentence: List[str], rows: int, cols: int) -> int:
+		i, n = 0, len(sentence)
+		r, c = rows, cols
+		lengths = [len(word) for word in sentence]
+
+		if max(lengths) > cols:
+			return 0
+
+		ans = 0
+		dp = {}
+		nxt = {}
+		for r in range(rows):
+			if i not in dp:
+				dp[i] = 0
+				stops = [sum(x) for x in zip(lengths, range(n))]
+				while bisect_left(stops, cols) == n:
+					cols -= 
+				print(i_)
+				if i_ == n:
+
+				nxt[i] = bisect_left(stops, cols)
+				ans += dp[i]
+			else:
+				ans += dp[i]
+				i = nxt[i]
+		return ans
+
+
+
+
+
+
+print(Solution().wordsTyping(["f","p","a"], 8, 7))
