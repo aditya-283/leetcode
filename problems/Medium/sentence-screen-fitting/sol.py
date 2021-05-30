78d80d9b3ff0b71ebaf48d2f3b3c8d1727bfb579
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-30 15:57:47 +0530

    Complete solution.

--- problems/Medium/sentence-screen-fitting/sol.py
+++ problems/Medium/sentence-screen-fitting/sol.py
@@ -5,26 +5,22 @@ from bisect import bisect_left
 class Solution:
 	def wordsTyping(self, sentence: List[str], rows: int, cols: int) -> int:
 		def wordsFit(i):
-			spaceTaken = sentence[i]
+			c = cols
 			count = 0
-			while spaceTaken < cols:
+			while len(sentence[i]) <= c:
+				c -= len(sentence[i]) + int(len(sentence[i]) < c)
 				i = (i + 1) % len(sentence)
 				count += 1
-				spaceTaken += len(sentence[i]) + 1
 			return count
 
-		wv = {}
+		wc = {}
 		for i in range(len(sentence)):
 			wc[i] = wordsFit(i)
 
 		words, i = 0, 0
 		for r in range(rows):
 			words += wc[i]
-			i += wc[i]
+			i = (i + wc[i]) % len(sentence)
 		return words // len(sentence)
-
-
-
-
 
 print(Solution().wordsTyping(["f","p","a"], 8, 7))
