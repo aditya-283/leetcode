e821824bb00014a378c6f68d653c409c4215f9f2
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-05-31 20:35:38 +0530

    Complete solution.

--- problems/Medium/delete-nodes-and-return-forest/sol.py
+++ problems/Medium/delete-nodes-and-return-forest/sol.py
@@ -1,1 +1,17 @@
 from typing import List
+
+class Solution:
+	def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
+		forest = []
+		to_delete = set(to_delete)
+		def delNodesRec(root):
+			if not root: return None
+			root.left = delNodesRec(root.left)
+			root.right = delNodesRec(root.right)
+			if root.val in to_delete:
+				if root.left: forest.append(root.left)
+				if root.right: forest.append(root.right)
+				return None
+			else: return root
+		if delNodesRec(root): forest.append(root)
+		return forest
--- problems/Medium/max-area-of-island/sol.py
+++ problems/Medium/max-area-of-island/sol.py
@@ -6,10 +6,10 @@ class Solution:
 	def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 		R, C = len(grid), len(grid[0])
 		def nbrs(i, j):
-			return ((r, c) for r, c in ((i+1, j), (i-1, j), (i, j-1), (i, j+1)) if 0 <= i < R and 0 <= j < C and grid[i][j] == 1)
+			return ((r, c) for r, c in ((i+1, j), (i-1, j), (i, j-1), (i, j+1)) if 0 <= r < R and 0 <= c < C and grid[r][c] == 1)
 
 		def bfs(s):
-			cnt = 0
+			cnt = 1
 			visited.add(s)
 			q = deque([s])
 			while q:
@@ -19,10 +19,9 @@ class Solution:
 						visited.add(nbr)
 						q.appendleft(nbr)
 						cnt += 1
-			print(cnt, 'for source', s)
 			return cnt
 
 		visited = set()
-		return max(bfs(s) for s in product(range(R), range(C)) if s not in visited)
+		return max(0, 0, *(bfs((r, c)) for r, c in product(range(R), range(C)) if (r, c) not in visited and grid[r][c] == 1))
 
-print(Solution().maxAreaOfIsland(grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))from typing import List
+print(Solution().maxAreaOfIsland(grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
