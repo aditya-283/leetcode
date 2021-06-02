1badbd788aefeff63aa5118f1eb797bc26cd02cf
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-06-02 20:18:09 +0530

    Complete solution.

--- problems/Medium/number-of-good-ways-to-split-a-string/sol.py
+++ problems/Medium/number-of-good-ways-to-split-a-string/sol.py
@@ -1,1 +1,17 @@
-from typing import List
+from collections import defaultdict, Counter
+
+class Solution:
+	def numSplits(self, s: str) -> int:
+		c, d, ans = defaultdict(int), Counter(s), 0
+		for i in range(len(s)-1):
+			c[s[i]] += 1
+			d[s[i]] -= 1
+			if d[s[i]] == 0:
+				del d[s[i]]
+
+			if len(c.keys()) == len(d.keys()):
+				ans += 1
+		return ans
+
+
+print(Solution().numSplits(s = "aacaba"))
